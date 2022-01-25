e9911f7a449a48b0ac5f1a8999425b51ef7179ed
Anhtuanzzz <62417928+Anhtuanzzz@users.noreply.github.com>
2022-01-25 11:21:17 +0700

    don't know what I'm doing

--- bot.js
+++ bot.js
@@ -1,23 +1,26 @@
 require('dotenv').config();
 const { Client, Intents } = require('discord.js');
 
-const client = new Client( {
-    intents: [
-        Intents.FLAGS.GUILDS,
-        Intents.FLAGS.GUILD_MESSAGES,
-    ],
-});
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 client.once('ready', () => {
-    console.log('Klara is waking');
+    console.log('Klara đã dậy rồi thưa onii-chan!');
 });
 
-client.on('messageCreate', message => {
-    if (message.content.startsWith('>')) {
-        if (message.content.substring(1) === 'hello') {
-            message.reply('Xin chao');
-        }
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) {
+        return;
     }
+
+	const { commandName } = interaction;
+
+	if (commandName === 'hello') {
+		await interaction.reply('Em chào onii-chan uwu!');
+	} else if (commandName === 'server') {
+		await interaction.reply('Server info.');
+	} else if (commandName === 'user') {
+		await interaction.reply('User info.');
+	}
 });
 
 client.login(process.env.TOKEN);
